4d0ef276941bdf8c8361177fcce82e83771bdf13
keith brooks <kbrook10@gmail.com>
2017-01-11 16:28:47 -0500

    Converted App component to a stateful component so that it could dynamically change

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import Header from './Header';
 
 //This Creates the App Component
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests via Modules!" />
-      <div>
-        ...
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageHeader: 'Naming Contests',
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message={ this.state.pageHeader } />
+        <div>
+          { this.state.test }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
