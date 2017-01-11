91f536a326b3205781e80c3b80842f96b4282ae0
keith brooks <kbrook10@gmail.com>
2017-01-11 15:55:11 -0500

    Create separate modules for components to isolate area of responsibility

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+//This Creates the App Component
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests via Modules!" />
+      <div>
+        ...
+      </div>
+    </div>
+  );
+};
+
+export default App;
