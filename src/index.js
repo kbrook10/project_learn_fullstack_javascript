accac508f1f3f8632c6f54479ee3e2eb468465cb
keith brooks <kbrook10@gmail.com>
2017-01-11 15:30:42 -0500

    Creating Header component to use within the App compoment

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//This Creates the App Component
-const App = (props) => {
+
+const Header = ({ message }) => {
   return (
-        <h2 className="text-center">
-            { props.headerMessage }
-        </h2>
+    <h2 className="Header text-center">
+      { message }
+    </h2>
+  );
+};
+
+//This Creates the App Component
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests!" />
+      <div>
+        ...
+      </div>
+    </div>
   );
 };
 
 //This is the Props validation
-App.propTypes = {
-  headerMessage: React.PropTypes.string
+Header.propTypes = {
+  message: React.PropTypes.string
 };
 
 //This allows us to implement default values
-App.defaultProps = {
-  headerMessage: 'Hello!'
+Header.defaultProps = {
+  message: 'Hello!'
 };
-
 
 //This renders the App Component to a location with ID of root in our view file.
 ReactDom.render(
